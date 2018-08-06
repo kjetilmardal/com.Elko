417a3f50a3146d73ba6646df877ae996b5b0a53c
prj84 <35060010+prj84@users.noreply.github.com>
2018-08-06 16:01:59 +0200

    Update driver.js

--- drivers/ESH Super TR RF Termostat/driver.js
+++ drivers/ESH Super TR RF Termostat/driver.js
@@ -1,1 +1,27 @@
-test
+{
+  "id": "ESH Super TR RF Termostat",
+  "name": {
+    "en": "ESH Super TR RF Termostat",
+  },
+  "class": "thermostat",
+  "capabilities": [
+    "target_temperature", "measure_temperature"],
+    
+  "zigbee": {
+    "manufacturerName": "ELKO",
+    "productId": "Super TR",
+    "deviceId": 0301,
+    "profileId": 0104,
+    "learnmode": {
+      "image": "/drivers/ESH Super TR RF Termostat/assets/include.svg",
+      "instruction": {
+        "en": "Click on and hold arrow up and M Button (1) until joining/pairing is displayed. Wait for Homey to list device",
+      }
+    }
+  },
+  "images": {
+    "large": "/drivers/ESH 316GLED Dimmer RF PF/assets/images/large.png",
+    "small": "/drivers/ESH 316GLED Dimmer RF PF/assets/images/small.png"
+  },
+  
+}
