f786fb5fc6c34a6d81011f585ccc5d0069dd8d83
Pål Raymond Johansen <35060010+prj84@users.noreply.github.com>
2018-08-17 12:45:07 +0200

    Add files via upload

--- drivers/ESH316GLED/device.js
+++ drivers/ESH316GLED/device.js
@@ -13,33 +13,21 @@ class ESH316GLED  extends ZigBeeDevice {
 		this.printNode();
 
 		if (this.hasCapability('onoff')) this.registerCapability('onoff', 'genOnOff');
-		this.registerAttrReportListener('genOnOff', 'onOff', 1, 0, 1, value => {
+		this.registerAttrReportListener('genOnOff', 'onOff', 1, 3600, 1, value => {
 			this.log('onoff', value);
 			this.setCapabilityValue('onoff', value === 1);
 		}, 0);
 
 		if (this.hasCapability('dim')) this.registerCapability('dim', 'genLevelCtrl');
-		this.registerAttrReportListener('genLevelCtrl', 'currentLevel', 1, 300, 2, value => {
+		this.registerAttrReportListener('genLevelCtrl', 'currentLevel', 1, 3600, 1, value => {
 			this.log('dim report', value);
 			this.setCapabilityValue('dim', value / 254);
 		}, 0);
 
   }
-	
 }
 
 module.exports = ESH316GLED;
-
-
-// report needed:
-// event name= level event value= 100
-// debug description is read attr - raw: DF4F01000808000020FE, dni: DF4F, endpoint: 01, cluster: 0008, size: 08, attrId: 0000, encoding: 20, value: fe
-//DF4F = Device network id
-//Value FE : dim, FE = 254, =100% (FF)
-//
-// ON/OFF status
-//debug event name= switch event value= off
-//debug description is on/off: 0
 
 //─────────────── Logging stdout & stderr ───────────────
 //2018-08-11 06:58:25 [log] [ElkoApp] Elko App is running!
